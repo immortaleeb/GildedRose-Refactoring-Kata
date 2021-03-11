a1728cfafd36c5e9a04e340c31047231a88cf556
Enver Bral <enver.bral@unifiedpost.com>
2021-03-11 16:58:56 +0100

    Promote InventoryItem to separate file

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -99,48 +99,4 @@ class GildedRose {
         new InventoryItem(item).dropQuality();
     }
 
-    private static class InventoryItem {
-        private final Item item;
-
-        public InventoryItem(Item item) {
-            this.item = item;
-        }
-
-        private boolean isLegendary() {
-            return item.name.equals("Sulfuras, Hand of Ragnaros");
-        }
-
-        private boolean isBackstagePass() {
-            return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-        }
-
-        private boolean isAgedBrie() {
-            return item.name.equals("Aged Brie");
-        }
-
-        private void decreaseSellIn() {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        private boolean isExpired() {
-            return item.sellIn < 0;
-        }
-
-        private void increaseQuality() {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-        }
-
-        private void decreaseQuality() {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
-
-        private void dropQuality() {
-            item.quality = 0;
-        }
-    }
-
 }
---
+++ Java/src/main/java/com/gildedrose/InventoryItem.java
@@ -0,0 +1,45 @@
+package com.gildedrose;
+
+class InventoryItem {
+    private final Item item;
+
+    public InventoryItem(Item item) {
+        this.item = item;
+    }
+
+    public boolean isLegendary() {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    public boolean isBackstagePass() {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    public boolean isAgedBrie() {
+        return item.name.equals("Aged Brie");
+    }
+
+    public void decreaseSellIn() {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    public boolean isExpired() {
+        return item.sellIn < 0;
+    }
+
+    public void increaseQuality() {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    public void decreaseQuality() {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    public void dropQuality() {
+        item.quality = 0;
+    }
+}
