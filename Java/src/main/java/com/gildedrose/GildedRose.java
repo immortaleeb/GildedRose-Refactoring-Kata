7f01a2df1c352fab3bd09ac1ace2c121be7abfd5
Enver Bral <enver.bral@unifiedpost.com>
2021-03-11 16:25:41 +0100

    Switch order of ifs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -47,8 +47,8 @@ class GildedRose {
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (item.quality > 0) {
                             decreaseQuality(item);
                         }
                     }
