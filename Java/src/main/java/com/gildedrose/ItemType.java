625db360586894bcdfd8f544dd2d9c7c5a865b2e
Enver Bral <enver.bral@unifiedpost.com>
2021-03-12 10:22:53 +0100

    Extract parse() method

---
+++ Java/src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,9 @@
+package com.gildedrose;
+
+public enum ItemType {
+    AGED_BRIE,
+    BACKSTAGE_PASS,
+    LEGENDARY_ITEM,
+    NORMAL_ITEM,
+    ;
+}
--- Java/src/main/java/com/gildedrose/ItemTypeParser.java
+++ Java/src/main/java/com/gildedrose/ItemTypeParser.java
@@ -3,15 +3,27 @@ package com.gildedrose;
 public class ItemTypeParser {
 
     public boolean isLegendary(String name) {
-        return name.equals("Sulfuras, Hand of Ragnaros");
+        return parse(name).equals(ItemType.LEGENDARY_ITEM);
     }
 
     public boolean isBackstagePass(String name) {
-        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+        return parse(name).equals(ItemType.BACKSTAGE_PASS);
     }
 
     public boolean isAgedBrie(String name) {
-        return name.equals("Aged Brie");
+        return parse(name).equals(ItemType.AGED_BRIE);
+    }
+
+    private ItemType parse(String name) {
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return ItemType.LEGENDARY_ITEM;
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return ItemType.BACKSTAGE_PASS;
+        } else if (name.equals("Aged Brie")) {
+            return ItemType.AGED_BRIE;
+        } else {
+            return ItemType.NORMAL_ITEM;
+        }
     }
 
 }
